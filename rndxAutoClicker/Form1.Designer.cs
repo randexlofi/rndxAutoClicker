66d09481e1c734b285ab2fc1ee4b4baaae8bd1d7
José Antunes <Jos Antunes@DESKTOP-ILT0ITU>
2025-01-24 23:56:31 +0000

    Adicionar arquivos de projeto.

---
+++ rndxAutoClicker/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace rndxAutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxMouseButton = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxHold = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.msInput = new System.Windows.Forms.TextBox();
+            this.buttonStartMacro = new System.Windows.Forms.Button();
+            this.buttonStopMacro = new System.Windows.Forms.Button();
+            this.timerAutoClicker = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonAssignKey = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label1.Name = "label1";
+            // 
+            // comboBoxMouseButton
+            // 
+            this.comboBoxMouseButton.BackColor = System.Drawing.Color.White;
+            this.comboBoxMouseButton.CausesValidation = false;
+            this.comboBoxMouseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxMouseButton.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMouseButton.FormattingEnabled = true;
+            this.comboBoxMouseButton.Items.AddRange(new object[] {
+            resources.GetString("comboBoxMouseButton.Items"),
+            resources.GetString("comboBoxMouseButton.Items1"),
+            resources.GetString("comboBoxMouseButton.Items2")});
+            resources.ApplyResources(this.comboBoxMouseButton, "comboBoxMouseButton");
+            this.comboBoxMouseButton.Name = "comboBoxMouseButton";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Name = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxHold);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBoxMouseButton);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBoxHold
+            // 
+            resources.ApplyResources(this.checkBoxHold, "checkBoxHold");
+            this.checkBoxHold.ForeColor = System.Drawing.Color.White;
+            this.checkBoxHold.Name = "checkBoxHold";
+            this.checkBoxHold.UseVisualStyleBackColor = true;
+            this.checkBoxHold.CheckedChanged += new System.EventHandler(this.checkBoxHold_CheckedChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Name = "label3";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.msInput);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Name = "label4";
+            // 
+            // msInput
+            // 
+            this.msInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.msInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.msInput, "msInput");
+            this.msInput.ForeColor = System.Drawing.Color.White;
+            this.msInput.Name = "msInput";
+            // 
+            // buttonStartMacro
+            // 
+            this.buttonStartMacro.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonStartMacro, "buttonStartMacro");
+            this.buttonStartMacro.Name = "buttonStartMacro";
+            this.buttonStartMacro.UseVisualStyleBackColor = true;
+            this.buttonStartMacro.Click += new System.EventHandler(this.buttonStartMacro_Click);
+            // 
+            // buttonStopMacro
+            // 
+            this.buttonStopMacro.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.buttonStopMacro, "buttonStopMacro");
+            this.buttonStopMacro.Name = "buttonStopMacro";
+            this.buttonStopMacro.UseVisualStyleBackColor = true;
+            this.buttonStopMacro.Click += new System.EventHandler(this.buttonStopMacro_Click);
+            // 
+            // timerAutoClicker
+            // 
+            this.timerAutoClicker.Interval = 1000;
+            this.timerAutoClicker.Tick += new System.EventHandler(this.AutoClicker_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::rndxAutoClicker.Properties.Resources.f6b8a3c5013ac8777f09b1463ead4b17;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PotatoLinkToGithub);
+            // 
+            // buttonAssignKey
+            // 
+            this.buttonAssignKey.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonAssignKey, "buttonAssignKey");
+            this.buttonAssignKey.Name = "buttonAssignKey";
+            this.buttonAssignKey.UseVisualStyleBackColor = true;
+            this.buttonAssignKey.Click += new System.EventHandler(this.buttonAssignKey_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.buttonAssignKey);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonStopMacro);
+            this.Controls.Add(this.buttonStartMacro);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxMouseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxHold;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox msInput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStartMacro;
+        private System.Windows.Forms.Button buttonStopMacro;
+        private System.Windows.Forms.Timer timerAutoClicker;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonAssignKey;
+    }
+}
+
---
+++ rndxAutoClicker/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+using System.Security.Policy;
+using System.Diagnostics;
+using System.Windows.Forms.VisualStyles;
+
+
+namespace rndxAutoClicker
+{
+    public partial class Form1 : Form
+    {
+        private bool isStarted = false;
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int MOD_NONE = 0x0000;
+        private const int WM_HOTKEY = 0x0312;
+        private const int HOTKEY_ID = 1;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifier, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private Keys assignedKey;
+        private bool isWaitingForKey = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_NONE, (int)assignedKey); // Register the key to Enable/Disable Macros.
+
+            comboBoxMouseButton.SelectedIndex = 0; // Default "Left" on Mouse Button Dropdown Menu.
+
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private class AppConfig
+        {
+            public bool isHoldClickChecked { get; set; }
+            public int comboBoxClickButton { get; set; }
+            public string textBoxMiliseconds { get; set; }
+            public Keys assignedKeyStored { get; set; }
+        }
+
+        private void SaveAppState()
+        {
+            var config = new AppConfig
+            {
+                isHoldClickChecked = checkBoxHold.Checked,
+                comboBoxClickButton = comboBoxMouseButton.SelectedIndex,
+                textBoxMiliseconds = msInput.Text,
+                assignedKeyStored = assignedKey
+            };
+
+            string json = JsonSerializer.Serialize(config);
+            File.WriteAllText("config.json", json);
+        }
+
+        private void LoadAppState()
+        {
+            if (File.Exists("config.json"))
+            {
+                string json = File.ReadAllText("config.json");
+                var config = JsonSerializer.Deserialize<AppConfig>(json);
+
+                checkBoxHold.Checked = config.isHoldClickChecked;
+                comboBoxMouseButton.SelectedIndex = config.comboBoxClickButton;
+                msInput.Text = config.textBoxMiliseconds;
+                assignedKey = config.assignedKeyStored;
+                RegisterHotKey(this.Handle, HOTKEY_ID, MOD_NONE, (int)assignedKey);
+                buttonStartMacro.Text = "Start (" + assignedKey.ToString() + ")";
+                buttonStopMacro.Text = "Stop (" + assignedKey.ToString() + ")";
+            }
+            else
+            {
+                // Default settings
+                checkBoxHold.Checked = false;
+                comboBoxMouseButton.SelectedIndex = 0;
+                msInput.Text = "1000";
+
+                //Set some more things up:
+                buttonStartMacro.Text = "Start";
+                buttonStopMacro.Text = "Stop";
+                buttonStartMacro.Enabled = false;
+                buttonStopMacro.Enabled = false;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                // Perform your action when F6 is pressed
+                StartStopMacro();
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SaveAppState();
+
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+
+            base.OnFormClosing(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            LoadAppState();
+
+            base.OnLoad(e);
+        }
+
+        void StartStopMacro()
+        {
+            if (isStarted)
+            {
+                StopMacro();
+            }
+            else
+            {
+                StartMacro();
+            }
+        }
+
+        private void StartMacro()
+        {
+            if (!checkBoxHold.Checked)
+            {
+                //Start Timer (Auto Clicker)
+                timerAutoClicker.Interval = (int)(Int32.Parse(msInput.Text));
+                timerAutoClicker.Start();
+            }
+            else
+            {
+                if (comboBoxMouseButton.SelectedIndex == 0) //Left Click
+                {
+                    mouse_event(0x0002, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 1) //Right Click
+                {
+                    mouse_event(0x0008, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 2) //Middle Click
+                {
+                    mouse_event(0x0020, 0, 0, 0, 0);
+                }
+            }
+
+            //Disable the StartMacro Button, and enable the StopMacro one.
+            buttonStartMacro.Enabled = false;
+            buttonStopMacro.Enabled = true;
+            buttonStartMacro.Cursor = Cursors.Arrow;
+            buttonStopMacro.Cursor = Cursors.Hand;
+
+            isStarted = true;
+        }
+
+        private void StopMacro()
+        {
+            if (!checkBoxHold.Checked)
+            {
+                //Stop Timer (Auto Clicker)
+                timerAutoClicker.Stop();
+            }
+            else
+            {
+                if (comboBoxMouseButton.SelectedIndex == 0) //Left Click
+                {
+                    mouse_event(0x0004, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 1) //Right Click
+                {
+                    mouse_event(0x0010, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 2) //Middle Click
+                {
+                    mouse_event(0x0040, 0, 0, 0, 0);
+                }
+            }
+
+            //Disable the StopMacro Button, and enable the StartMacro one.
+            buttonStartMacro.Enabled = true;
+            buttonStopMacro.Enabled = false;
+            buttonStartMacro.Cursor = Cursors.Hand;
+            buttonStopMacro.Cursor = Cursors.Arrow;
+
+            isStarted = false;
+        }
+
+        private void buttonStartMacro_Click(object sender, EventArgs e)
+        {
+            StartMacro();
+        }
+
+        private void buttonStopMacro_Click(object sender, EventArgs e)
+        {
+            StopMacro();
+        }
+
+        private void AutoClicker_Tick(object sender, EventArgs e)
+        {
+            if (!Bounds.Contains(PointToClient(MousePosition)))
+            {
+                if (comboBoxMouseButton.SelectedIndex == 0) //Left Click
+                {
+                    mouse_event(0x0002, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(10);
+                    mouse_event(0x0004, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 1) //Right Click
+                {
+                    mouse_event(0x0008, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(10);
+                    mouse_event(0x0010, 0, 0, 0, 0);
+                }
+                else if (comboBoxMouseButton.SelectedIndex == 2) //Middle Click
+                {
+                    mouse_event(0x0020, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(10);
+                    mouse_event(0x0040, 0, 0, 0, 0);
+                }
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (isWaitingForKey)
+            {
+                UnregisterHotKey(this.Handle, HOTKEY_ID); // Unregister the previous Key.
+
+                assignedKey = e.KeyCode;
+                buttonStartMacro.Text = "Start (" + assignedKey.ToString() + ")";
+                buttonStopMacro.Text = "Stop (" + assignedKey.ToString() + ")";
+
+                RegisterHotKey(this.Handle, HOTKEY_ID, MOD_NONE, (int)assignedKey);
+
+                isWaitingForKey = false;
+                buttonAssignKey.Enabled = true;
+                buttonStartMacro.Enabled = true;
+                buttonStopMacro.Enabled = false;
+
+                if(!checkBoxHold.Checked)
+                    msInput.Enabled = true;
+                else 
+                    msInput.Enabled = false;
+            }
+        }
+
+        private void buttonAssignKey_Click(object sender, EventArgs e)
+        {
+            isWaitingForKey = true;
+            buttonAssignKey.Enabled = false;
+
+            StopMacro();
+            buttonStartMacro.Enabled = false;
+            buttonStopMacro.Enabled = false;
+
+            msInput.Enabled = false;
+        }
+
+        private void checkBoxHold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxHold.Checked)
+                msInput.Enabled = false;
+            else
+                msInput.Enabled = true;
+        }
+
+        private void PotatoLinkToGithub(object sender, EventArgs e)
+        {
+            string githubUrl = "https://github.com/randexlofi";
+
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = githubUrl,
+                    UseShellExecute = true, //Ensure the link is opened in the default browser.
+                });
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open the link: " + ex.Message);
+            }
+        }
+    }
+}
